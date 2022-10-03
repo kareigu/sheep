9e7270830d9929a32ac89b032f723fd1931a30d5
karei <eetu.kivelae@gmail.com>
2022-10-03 14:15:02 +0300

    Add rust-fmt

--- src/subscription.rs
+++ src/subscription.rs
@@ -1,11 +1,10 @@
 use std::sync::Arc;
 
-use serde::{Serialize, Deserialize};
-use serenity::model::prelude::{GuildId, ChannelId};
+use reddb::{serializer::Ron, FileStorage, RedDb};
+use serde::{Deserialize, Serialize};
+use serenity::model::prelude::{ChannelId, GuildId};
 use serenity::prelude::{Context, TypeMapKey};
-use reddb::{RedDb, FileStorage, serializer::Ron};
-use tracing::{info, debug, error, warn};
-
+use tracing::{debug, error, info, warn};
 
 #[derive(Clone, Serialize, PartialEq, Deserialize, Debug)]
 pub struct Subscription {
@@ -20,12 +19,8 @@ pub enum SubscriptionHandleResult {
 }
 
 impl Subscription {
-
   pub fn new(guild: GuildId, channel: ChannelId) -> Self {
-    Self {
-      guild,
-      channel,
-    }
+    Self { guild, channel }
   }
 
   pub async fn format_to_string(&self, ctx: &Context, subscribed: bool) -> String {
@@ -45,52 +40,48 @@ impl Subscription {
       false => "Unsubscribed from",
     };
 
-    format!("{} {} in {}",
-      verb,
-      channel_name,
-      guild_name
-    )
+    format!("{} {} in {}", verb, channel_name, guild_name)
   }
 
-  pub async fn create_and_handle(ctx: &Context, guild_id: Option<GuildId>, channel_id: ChannelId) -> SubscriptionHandleResult {
+  pub async fn create_and_handle(
+    ctx: &Context,
+    guild_id: Option<GuildId>,
+    channel_id: ChannelId,
+  ) -> SubscriptionHandleResult {
     if let Some(guild) = guild_id {
       let subscription = Subscription::new(guild, channel_id);
-      
+
       subscription.handle(&ctx).await
     } else {
       SubscriptionHandleResult::Error("No GuildId provided".to_string())
     }
   }
-  
+
   async fn handle(self, ctx: &Context) -> SubscriptionHandleResult {
-    let db = { 
+    let db = {
       let data = ctx.data.read().await;
       if let Some(handle) = data.get::<Subscriptions>() {
         handle.clone()
       } else {
-        return SubscriptionHandleResult::Error("Database not in shared data".to_string())
+        return SubscriptionHandleResult::Error("Database not in shared data".to_string());
       }
     };
-
 
     let deleted = match db.delete(&self).await {
       Ok(count) => {
         debug!("Removed {} subscriptions", count);
         count != 0
-      },
-      Err(e) => {
-        return SubscriptionHandleResult::Error(format!("Error removing: {}", e))
-      },
+      }
+      Err(e) => return SubscriptionHandleResult::Error(format!("Error removing: {}", e)),
     };
 
     if deleted {
-      return SubscriptionHandleResult::Removed(self.clone())
+      return SubscriptionHandleResult::Removed(self.clone());
     }
 
-
-    debug!("Adding subscription for GuildID({}) in ChannelId({})", 
-      self.guild, 
-      self.channel
+    debug!(
+      "Adding subscription for GuildID({}) in ChannelId({})",
+      self.guild, self.channel
     );
     if let Err(e) = db.insert_one(self.clone()).await {
       return SubscriptionHandleResult::Error(format!("Error inserting: {}", e));
@@ -101,7 +92,6 @@ impl Subscription {
 }
 
 pub struct Subscriptions;
-
 
 impl TypeMapKey for Subscriptions {
   type Value = Arc<RedDb<Ron, FileStorage<Ron>>>;
--- src/utils.rs
+++ src/utils.rs
@@ -1,22 +1,20 @@
 use serenity::{
-  Error,
-  prelude::Context, 
-  model::prelude::interaction::application_command::ApplicationCommandInteraction
+  model::prelude::interaction::application_command::ApplicationCommandInteraction,
+  prelude::Context, Error,
 };
 
-
 pub async fn text_response<D>(
-  ctx: &Context, 
-  command: ApplicationCommandInteraction, text: D) -> Result<(), Error>
-where D: ToString, {
+  ctx: &Context,
+  command: ApplicationCommandInteraction,
+  text: D,
+) -> Result<(), Error>
+where
+  D: ToString,
+{
   let _a = command
     .edit_original_interaction_response(&ctx.http, |response| {
-      response
-        .embed(|embed| {
-          embed
-            .title(text)
-            .colour(0xFFFFFF)
-        })
-    }).await?;
+      response.embed(|embed| embed.title(text).colour(0xFFFFFF))
+    })
+    .await?;
   Ok(())
 }
